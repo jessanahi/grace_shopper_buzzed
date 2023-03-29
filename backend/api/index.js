ceea4110f8be85e101a91de38c24e2e9c4834456
jessanahi <jess.anahi.zamora@gmail.com>
2023-03-28 21:24:11 -0500

    deploy attempt 3

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const cors = require('cors');
+router.use(cors());
 // GET /api/health
 router.get('/health', async (req, res, next) => {
       res.send({
